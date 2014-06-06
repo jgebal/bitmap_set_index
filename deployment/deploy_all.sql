a61c01ac9924f2961c18bf7047b65c7b702eb34b
Jacek Gębal <jgebal@gmail.com>
2014-06-07 01:54:42 +0200

    Reorganized directory structure. Added deploy_all script

---
+++ deployment/deploy_all.sql
@@ -0,0 +1,13 @@
+@@../sources/mapping_key_list.tps
+@@../sources/int_list.tps
+@@../sources/bmap_node_list.tps
+@@../sources/bmap_level_list.tps
+
+@@create_hierarchical_bitmap_sequence.sql
+@@create_hierarchical_bitmap_table.sql
+@@create_bitmap_mapping_table.sql
+
+@@../sources/bmap_util.pkb
+@@../sources/mapping_util.pks
+@@../sources/bmap_util.pkb
+@@../sources/mapping_util.pkb
