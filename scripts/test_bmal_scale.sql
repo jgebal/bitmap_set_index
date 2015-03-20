f7160a0a1f86527aab60d398674f2b9154ca7404
Jacek Gębal <jgebal@gmail.com>
2015-03-20 15:18:05 +0100

    Added missing function creation for test.

--- scripts/test_bmal_scale.sql
+++ scripts/test_bmal_scale.sql
@@ -1,4 +1,18 @@
 SET TIMING ON
+
+CREATE OR REPLACE FUNCTION bmap_list_generator(p_bits INTEGER, p_density INTEGER) RETURN INT_LIST PIPELINED IS
+  v_rows INTEGER := p_bits*p_density;
+  i      INTEGER := 1;
+  BEGIN
+    LOOP
+      PIPE ROW(TRUNC(i/p_density));
+      EXIT WHEN i > v_rows;
+      i := i + 1;
+    END LOOP;
+    RETURN;
+  END bmap_list_generator;
+/
+
 DECLARE
   c                       bmap_builder.BIT_LIST_REF_C;
   X                       INT_LIST;
