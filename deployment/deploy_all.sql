5d88dc2ecb0154ac0cb42f323ab131df14ca5b58
Jacek Gębal <jgebal@gmail.com>
2015-04-10 22:35:35 +0100

    removed obsolete sources

--- deployment/deploy_all.sql
+++ deployment/deploy_all.sql
@@ -1,4 +1,3 @@
-@@../sources/types/mapping_key_list.tps
 @@../sources/types/int_list.tps
 @@../sources/types/stor_bmap_node.tps
 @@../sources/types/stor_bmap_level.tps
@@ -9,9 +8,7 @@
 @@../sources/packages/bmap_segment_builder.pks
 @@../sources/packages/bmap_builder.pks
 @@../sources/packages/bmap_persist.pks
-@@../sources/packages/mapping_util.pks
 
 @@../sources/packages/bmap_segment_builder.pkb
 @@../sources/packages/bmap_builder.pkb
 @@../sources/packages/bmap_persist.pkb
-@@../sources/packages/mapping_util.pkb
