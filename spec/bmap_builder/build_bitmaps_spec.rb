849ddc0af395a5280b78233100df3ac8a3603ba2
Jacek Gębal <jgebal@gmail.com>
2015-04-26 14:42:54 +0100

    Added some more tests on building bitmaps

--- spec/bmap_builder/build_bitmaps_spec.rb
+++ spec/bmap_builder/build_bitmaps_spec.rb
@@ -34,16 +34,46 @@ describe 'Build and store bitmap index' do
     build_bitmap( storage_table_name, bitmap_key, bit_list )
     #then
 
-    expect( get_bmap_root_segment( storage_table_name, bitmap_key) ).to eq( one_bit_bitmap_segment )
-    expect( get_segment( storage_table_name, bitmap_key, 1, 2 ) ).to eq( two_bit_segment )
-    expect( get_segment( storage_table_name, bitmap_key, 2,1) ).to eq( full_bitmap_segment )
-    expect( get_segment( storage_table_name, bitmap_key, 1,1) ).to eq( full_bitmap_segment )
+    expect( get_segment( storage_table_name, bitmap_key, 1, 3) ).to eq( one_bit_bitmap_segment )
+    expect( get_segment( storage_table_name, bitmap_key, 1, 2) ).to eq( two_bit_segment )
+    expect( get_segment( storage_table_name, bitmap_key, 2, 1) ).to eq( full_bitmap_segment )
+    expect( get_segment( storage_table_name, bitmap_key, 1, 1) ).to eq( full_bitmap_segment )
 
     expect( count_segments( storage_table_name, bitmap_key) ).to eq(4)
 
   end
 
-  after(:all) do
+  it 'should create a bitmap for a single bit' do
+    bitmap_key = 1
+    bit_list = [plsql.bmap_builder.c_max_bitmap_size]
+    last_bit_set_segment = [
+        [{node_index: 900, node_value: 2**29}],
+        [{node_index:  30, node_value: 2**29}],
+        [{node_index:   1, node_value: 2**29}]
+    ]
+    build_bitmap( storage_table_name, bitmap_key, bit_list )
+    expect( get_segment( storage_table_name, bitmap_key, 1, 3) ).to eq( last_bit_set_segment )
+    expect( get_segment( storage_table_name, bitmap_key, 27000, 2) ).to eq( last_bit_set_segment )
+    expect( get_segment( storage_table_name, bitmap_key, 27000**2, 1) ).to eq( last_bit_set_segment )
+
+    expect( count_segments( storage_table_name, bitmap_key) ).to eq(3)
+
+  end
+
+  it 'should create a bitmap for a maximum allowed value of bit pos' do
+    bitmap_key = 1
+    bit_list = [1]
+    first_bit_set_segment = [
+        [{node_index: 1, node_value: 1}],
+        [{node_index: 1, node_value: 1}],
+        [{node_index: 1, node_value: 1}]
+    ]
+    build_bitmap( storage_table_name, bitmap_key, bit_list )
+    expect( get_segment( storage_table_name, bitmap_key, 1, 3) ).to eq( first_bit_set_segment )
+    expect( get_segment( storage_table_name, bitmap_key, 1, 2) ).to eq( first_bit_set_segment )
+    expect( get_segment( storage_table_name, bitmap_key, 1, 1) ).to eq( first_bit_set_segment )
+
+    expect( count_segments( storage_table_name, bitmap_key) ).to eq(3)
 
   end
 
