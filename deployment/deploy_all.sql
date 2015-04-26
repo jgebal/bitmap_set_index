96c0865fcf420c12337a9915680faf071f7ad379
Jacek Gębal <jgebal@gmail.com>
2015-04-26 10:48:25 +0100

    Moved compilation settings to deployment script.

--- deployment/deploy_all.sql
+++ deployment/deploy_all.sql
@@ -3,6 +3,13 @@ DEF SEGMENT_HEIGHT__DEF_VAL_IS_3=3;
 --THE SEGMENT ELEMENTS must be greater than or eaqual to the (C_ELEMENT_CAPACITY ^ C_SEGMENT_HEIGHT - 1)
 DEF SEGMENT_ELEMS__DEF_VAL_IS_900=900;
 
+ALTER SESSION SET PLSQL_WARNINGS = 'ENABLE:ALL';
+
+ALTER SESSION SET PLSQL_CODE_TYPE = NATIVE;
+/
+ALTER SESSION SET PLSQL_OPTIMIZE_LEVEL = 3;
+/
+
 @@../sources/types/int_list.tps
 @@../sources/types/varchar2_lst.tps
 @@../sources/types/stor_bmap_node.tps
